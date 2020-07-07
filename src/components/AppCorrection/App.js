a3eedd624178f8ef3986bbd64b939b2ef46b874c
alex <desorbaix@free.fr>
2020-07-08 00:47:50 +0200

    correction

--- src/components/AppCorrection/App.js
+++ src/components/AppCorrection/App.js
@@ -1,16 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import MemeFormEditor from './components/MemeFormEditor/MemeFormEditor';
-import MemeSvgViewer from './components/MemeSvgViewer/MemeSvgViewer';
+import MemeFormEditor from '../MemeFormEditor/MemeFormEditor';
+import MemeSvgViewer from '../MemeSvgViewer/MemeSvgViewer';
+import Banner from '../Banner/Banner';
+import Footer from '../Footer/Footer';
 
-function App() {
-  return (
-    <div className="App">
-      <MemeSvgViewer meme={{selectedImg:{url:'seigneur.jpg',id:0},texts:[{x:50,y:32,value:'devenir pro react',fontSize:10}],temporaryText:{x:50,y:75,value:'easy',fontSize:10}}}/>  
-      <MemeFormEditor/>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) { super(props); this.state = {}; }
+  render() {
+    return (
+      <>
+        <Banner></Banner>
+        <div className="App">
+          <MemeSvgViewer meme={{ selectedImg: { url: 'seigneur.jpg', id: 0 }, texts: [{ x: 50, y: 32, value: 'devenir pro react', fontSize: 10 }], temporaryText: { x: 50, y: 75, value: 'easy', fontSize: 10 } }} />
+          <MemeFormEditor />
+        </div>
+        <Footer></Footer>
+      </>
+    );
+  }
 }
 
 export default App;
--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -3,9 +3,11 @@ import PropTypes from 'prop-types';
 import styles from './Banner.module.scss';
 
 const Banner = () => (
+  <>
   <div className={styles.Banner} data-testid="Banner">
     Meme <span>.</span> Maker
   </div>
+<hr/></>
 );
 
 Banner.propTypes = {};
